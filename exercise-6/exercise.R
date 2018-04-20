491eeea4468ea2b623f1bf61c7419136d3adc372
hills97-1563950 <donghills1997@gmail.com>
2018-04-19 21:15:45 -0700

    DONE

--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -10,9 +10,24 @@ library(dplyr)
 # use `left_join()` to join on the "airports" dataframe, which has the airport
 # information
 # Which airport had the largest average arrival delay?
+largest_arrival_delay <- flights %>%
+  group_by(desk) %>%
+  summarize(avg_delay = mean(arr_delay, na.rm = TRUE)) %>%
+  mutate(faa = desk)
 
+joined <- full_join(largest_arrival_delay, airports, by = 'faa')
 
+  filter(avg_delay == max(avg_delay, na.rm = TRUE))
+largest_arrival_delay
 # Create a dataframe of the average arrival delay for each _airline_, then use
 # `left_join()` to join on the "airlines" dataframe
 # Which airline had the smallest average arrival delay?
+smallest_arrival_delay <- flights %>%
+  group_by(desk) %>%
+  summarize(avg_delay = mean(arr_delay, na.rm = TRUE)) %>%
+  mutate(faa = desk)
 
+joined <- full_join(largest_arrival_delay, airports, by = 'faa')
+
+filter(avg_delay == min(avg_delay, na.rm = TRUE))
+smallest_arrival_delay
